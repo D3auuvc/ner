a332ba1fb97e569894418a002fc7d1ee753d153e
D3annyC <chenghan.chung@ut.ee>
2022-04-24 16:14:15 +0300

    [Add] Added training tasks

--- run_ner_joint_lr1e-5_s5436.sh
+++ run_ner_joint_lr1e-5_s5436.sh
@@ -9,7 +9,7 @@
 #SBATCH --output=R-%x.%j.out
 
 module load python/3.8.6
-source py_venv/venv_s5436/bin/activate
+source py_venv/venv/bin/activate
 
 wandb login
 WANDB_PROJECT=EstNewNER
--- run_ner_joint_lr1e-5_s5490.sh
+++ run_ner_joint_lr1e-5_s5490.sh
@@ -2,18 +2,18 @@
 
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --job-name=j15s6523
+#SBATCH --job-name=j15s5490
 #SBATCH --mail-type=ALL
 #SBATCH --time 1-00:00:00
 #SBATCH --mem=20000
 #SBATCH --output=R-%x.%j.out
 
 module load python/3.8.6
-source py_venv/venv_s6523/bin/activate
+source py_venv/venv/bin/activate
 
 wandb login
 WANDB_PROJECT=EstNewNER
 
-CONFIG_PATH=config/config_ner_joint_lr1e-5_s6523.json
+CONFIG_PATH=config/config_ner_joint_lr1e-5_s5490.json
 
 python run_ner_main.py $CONFIG_PATH
